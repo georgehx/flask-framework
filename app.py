d42067f700787b4c2cf7161ca8a9291771c6b472
jokergeorge <georgehanxiao@msn.com>
2020-04-06 00:03:02 -0400

    plot try1

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -10,6 +11,26 @@ def index():
 @app.route('/about')
 def about():
   return render_template('about.html')
+
+@app.route('/plot')
+def plot():
+    ticker = request.form['name_lulu']
+    print(ticker)
+    apicall = 'https://www.quandl.com/api/v3/datasets/WIKI/FB/data.csv?column_index=4&start_date=2012-11-01&end_date=2013-11-30'
+    apikey = 'yRdMoLRR-tk-oNmDdQpd'
+    strcall = apicall + apikey
+
+
+
+    response = requests.get(strcall)
+    df = pd.read_csv(response)
+    df.plot.line()
+    return render_template('index.html')
+
+
+
+
+
 
 if __name__ == '__main__':
   app.run(port=33507)
