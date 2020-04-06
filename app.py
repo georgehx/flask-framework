575d535a1828004204e1d1da4e5add3e4852c7b7
jokergeorge <georgehanxiao@msn.com>
2020-04-06 01:33:11 -0400

    commit again

--- app.py
+++ app.py
@@ -32,9 +32,15 @@ def plot():
     #df.plot.line()
     img = io.BytesIO()
 
-    df.plot()
+    y = [1,2,3,4,5]
+    x = [0,2,1,3,4]
+    plt.plot(x,y)
     plt.savefig(img, format='png')
     img.seek(0)
+
+    plot_url = base64.b64encode(img.getvalue()).decode()
+
+    return '<img src="data:image/png;base64,{}">'.format(plot_url)
 
 
 
