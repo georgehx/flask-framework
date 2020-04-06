d5f08b13517c7468db707bbc88cf5b31e66a76c3
jokergeorge <georgehanxiao@msn.com>
2020-04-06 02:14:26 -0400

    commit again

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask, render_template, request, redirect
 import pandas as pd
 import requests, io
 import base64
-#from bokeh.embed import components
-#from bokeh.models import HoverTool
-#from bokeh.plotting import figure, output_file, show
 
 
 app = Flask(__name__)
@@ -12,30 +9,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-    #return 'Hello World?'
 
 @app.route('/about')
 def about():
-    #ticker = request.form['name_lulu']
-    #return render_template('about.html', name = ticker)
     return render_template('about.html')
 
-#Helper function
-#def get_plot(df):
-    #Make plot and customize
-#    p = figure()
-#    p.line(df, x='Time', y='Prices',title='Stock Prices')
-#    p.title.text_font_size = '16pt'
-#    p.add_tools(HoverTool()) #Need to configure tooltips for a good HoverTool
 
-    #Return the plot
-#    return(p)
 
 @app.route('/plot', methods=['GET', 'POST'])
 def plot():
     ticker = request.form['name_ticker']
     print('This is another test')
-    apicall = 'https://www.quandl.com/api/v3/datasets/WIKI/FB/data.csv?column_index=4&start_date=2012-11-01&end_date=2013-11-30'
+    apicall = 'https://www.quandl.com/api/v3/datasets/WIKI/'+ticker+'/data.csv?column_index=4&start_date=2012-11-01&end_date=2013-11-30'
     apikey = '&api_key=yRdMoLRR-tk-oNmDdQpd'
     strcall = apicall + apikey
 
@@ -46,11 +31,7 @@ def plot():
     prices = str(response.content)
     #return shape
 
-    #Setup plot
-#    p = get_plot(df)
-#    script, div = components(p)
-    #return render_template('about.html', script=script, div=div)
-    #return render_template('index.html')
+
     return prices
 
 
