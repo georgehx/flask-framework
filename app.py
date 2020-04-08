da059c5bd6e468db99dc479fb93a5f79e6e50767
jokergeorge <georgehanxiao@msn.com>
2020-04-08 15:54:24 -0400

    commit again

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request, redirect
 import pandas as pd
-import requests, io
+import requests, io, os
 import base64
-from bokeh.plotting import figure
 from bokeh.embed import components
+from bokeh.plotting import figure
+from bokeh.resources import INLINE
+from bokeh.util.string import encode_utf8
 
 app = Flask(__name__)
+
+
+
 
 @app.route('/')
 def index():
@@ -30,27 +35,49 @@ def plot():
     #df.plot.line()
     shape = str(df.shape[0])
     prices = str(response.content)
-    return prices
+    #return prices
 
-    #x = [1, 3, 5, 7]
-    #y = [2, 4, 6, 8]
-    #p = figure()
+    x = [1, 3, 5, 7]
+    y = [2, 4, 6, 8]
+    p = figure()
 
-    #p.line(x, y, color='blue', legend='line')
+    p.line(x, y, color='blue', legend='line')
     #Setup plot
 
-    #script, div = components(p)
+    script, div = components(p)
 
     #Render the page
-    #return render_template('about.html', script=script, div=div)
+    return render_template('about.html', script=script, div=div)
 
+@app.route('/bokeh')
+def bokeh():
+    ticker = request.form['name_ticker']
+    # init a basic bar chart:
+    # http://bokeh.pydata.org/en/latest/docs/user_guide/plotting.html#bars
+    fig = figure(plot_width=600, plot_height=600)
+    fig.vbar(
+        x=[1, 2, 3, 4],
+        width=0.5,
+        bottom=0,
+        top=[1.7, 2.2, 4.6, 3.9],
+        color='navy'
+    )
 
+    # grab the static resources
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
 
+    # render template
+    script, div = components(fig)
+    html = render_template(
+        'about.html',
+        plot_script=script,
+        plot_div=div,
+        js_resources=js_resources,
+        css_resources=css_resources,
+    )
+    return encode_utf8(html)
 
-
-@app.route('/index_lulu', methods=['GET', 'POST'])
-def index_lulu():
-    return 'Hello Index_lulu'
 
 
 
