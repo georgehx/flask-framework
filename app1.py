4ac151049fbce09229680504060b0ba3a44072af
jokergeorge <georgehanxiao@msn.com>
2020-04-08 23:54:46 -0400

    commit again

--- app1.py
+++ app1.py
@@ -10,28 +10,23 @@ import requests, io
 output_file('columndatasource_example.html')
 
 #ticker = request.form['name_ticker']
-ticker = 'FB'
+ticker = 'AAPL'
 apicall = 'https://www.quandl.com/api/v3/datasets/WIKI/'+ticker+'/data.csv?column_index=4&start_date=2012-11-01&end_date=2013-11-30'
 apikey = '&api_key=yRdMoLRR-tk-oNmDdQpd'
 strcall = apicall + apikey
 
 response = requests.get(strcall)
 df = pd.read_csv(io.BytesIO(response.content), delimiter = ',', sep = "\n")
+df['Date'] = pd.to_datetime(df['Date'])
 
+print(df.head(4))
 
-source = ColumnDataSource(df)
+#source = ColumnDataSource(df)
 
-p = figure()
-p.line(x='Date', y='Close',source=source)
+p1 = figure(x_axis_type="datetime", title="Data from Quandle WIKI set")
+# p1.grid.grid_line_alpha=0.3
+# p1.xaxis.axis_label = 'Date'
+# p1.yaxis.axis_label = 'Price'
 
-hover = HoverTool()
-hover.tooltips=[
-    ('Attack Date', '@MSNDATE'),
-    ('Attacking Aircraft', '@AC_ATTACKING'),
-    ('Tons of Munitions', '@TOTAL_TONS'),
-    ('Type of Aircraft', '@AIRCRAFT_NAME')
-]
-
-p.add_tools(hover)
-
-show(p)
+p1.line(df['Date'], df['Close'], color='#33A02C', legend= ticker+':Close')
+show(p1)
--- app2.py
+++ app2.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, request, redirect
+#column_datasource.py
+from flask import Flask, render_template
 import pandas as pd
-import requests, io, os
-import base64
-from bokeh.plotting import figure, output_file, show
 from bokeh.embed import components
-from bokeh.models import HoverTool
-from bokeh.charts import Scatter
+from bokeh.plotting import figure
+from bokeh.resources import INLINE
+from bokeh.util.string import encode_utf8
+import requests, io
+
+app2 = Flask(__name__)
 
 
 
 
-app = Flask(__name__)
+
 
 def get_stock(ticker):
     ticker = ticker
@@ -22,31 +24,35 @@ def get_stock(ticker):
     df = pd.read_csv(io.BytesIO(response.content), delimiter = ',', sep = "\n")
     df['Date'] = pd.to_datetime(df['Date'])
 
+    print(df.head(4))
     return df
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/plot', methods=['GET', 'POST'])
-def plot():
-    ticker = request.form['name_ticker']
-    apicall = 'https://www.quandl.com/api/v3/datasets/WIKI/'+ticker+'/data.csv?column_index=4&start_date=2012-11-01&end_date=2013-11-30'
-    apikey = '&api_key=yRdMoLRR-tk-oNmDdQpd'
-    strcall = apicall + apikey
-
-    response = requests.get(strcall)
-    df = pd.read_csv(io.BytesIO(response.content), delimiter = ',', sep = "\n")
-    df['Date'] = pd.to_datetime(df['Date'])
-
     #source = ColumnDataSource(df)
+
+    #p1 = figure(x_axis_type="datetime", title="Data from Quandle WIKI set")
+    # p1.grid.grid_line_alpha=0.3
+    # p1.xaxis.axis_label = 'Date'
+    # p1.yaxis.axis_label = 'Price'
+
+    #p1.line(df['Date'], df['Close'], color='#33A02C', legend= ticker+':Close')
+
+
+@app2.route('/')
+def index():
+    df = get_stock('FB')
+    return str(len(df))
+
+
+@app2.route('/bokeh')
+def bokeh():
+
+    # init a basic bar chart:
+    # http://bokeh.pydata.org/en/latest/docs/user_guide/plotting.html#bars
+    df = get_stock('FB')
     fig = figure(x_axis_type="datetime", title="Data from Quandle WIKI set")
     fig.line(df['Date'], df['Close'], color='#33A02C')
+
+
     # grab the static resources
     js_resources = INLINE.render_js()
     css_resources = INLINE.render_css()
@@ -63,6 +69,5 @@ def plot():
     return encode_utf8(html)
 
 
-
 if __name__ == '__main__':
-  app.run(port=33507)
+    app2.run(debug=True)
