0c8996fd4eea0fb965f5c4c404016b2e43bf15ad
jokergeorge <georgehanxiao@msn.com>
2020-04-08 23:58:08 -0400

    commit again

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request, redirect
+#column_datasource.py
+from flask import Flask, render_template
 import pandas as pd
-import requests, io, os
-import base64
-from bokeh.plotting import figure, output_file, show
 from bokeh.embed import components
-from bokeh.models import HoverTool
-from bokeh.charts import Scatter
+from bokeh.plotting import figure
+from bokeh.resources import INLINE
+from bokeh.util.string import encode_utf8
+import requests, io
 
 
 
@@ -36,13 +36,7 @@ def about():
 @app.route('/plot', methods=['GET', 'POST'])
 def plot():
     ticker = request.form['name_ticker']
-    apicall = 'https://www.quandl.com/api/v3/datasets/WIKI/'+ticker+'/data.csv?column_index=4&start_date=2012-11-01&end_date=2013-11-30'
-    apikey = '&api_key=yRdMoLRR-tk-oNmDdQpd'
-    strcall = apicall + apikey
-
-    response = requests.get(strcall)
-    df = pd.read_csv(io.BytesIO(response.content), delimiter = ',', sep = "\n")
-    df['Date'] = pd.to_datetime(df['Date'])
+    df = get_stock('FB')
 
     #source = ColumnDataSource(df)
     fig = figure(x_axis_type="datetime", title="Data from Quandle WIKI set")
