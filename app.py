f3750e1f73e6f742a7ccd171e97f02c800f5afb8
jokergeorge <georgehanxiao@msn.com>
2020-04-08 21:41:18 -0400

    commit again

--- app.py
+++ app.py
@@ -13,5 +13,20 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/plot', methods=['GET', 'POST'])
+def plot():
+    ticker = request.form['name_ticker']
+    apicall = 'https://www.quandl.com/api/v3/datasets/WIKI/'+ticker+'/data.csv?column_index=4&start_date=2012-11-01&end_date=2013-11-30'
+    apikey = '&api_key=yRdMoLRR-tk-oNmDdQpd'
+    strcall = apicall + apikey
+
+    response = requests.get(strcall)
+    df = pd.read_csv(io.BytesIO(response.content), delimiter = ',', sep = "\n")
+    prices = str(response.content)
+    return prices
+
+
+
+
 if __name__ == '__main__':
   app.run(port=33507)
